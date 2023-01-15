f32f9e93b16bc146defa69fdddffb5aec32d58e8
Furkan Ayyıldız <furkan199905@gmail.com>
2023-01-15 17:55:41 +0300

    Abenteeism  added

--- ubis_app/lib/screens/option_screens/absenteeism_screen.dart
+++ ubis_app/lib/screens/option_screens/absenteeism_screen.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import "../../fake_data.dart";
 
 class AbsenteeismScreen extends StatelessWidget {
+  final List<int> values = [20, 5, 16, 9, 12];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("sfdsgsghshf"),
-      ),
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: Text("Absenteeism"),
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+        body: ListView.builder(
+          itemCount: COURSE_DATA.length,
+          itemBuilder: (context, index) {
+            return Card(
+              elevation: 10,
+              margin: EdgeInsets.all(10),
+              child: ListTile(
+                  onTap: null,
+                  title: Text(COURSE_DATA[index].code),
+                  tileColor: Colors.grey.shade300,
+                  subtitle: Text(COURSE_DATA[index].name),
+                  trailing: Container(
+                    padding: EdgeInsets.all(5),
+                    child: Text("%" + values[index].toString()),
+                    color: values[index] > 15 ? Colors.red : Colors.green,
+                  )),
+            );
+          },
+        ));
   }
 }
