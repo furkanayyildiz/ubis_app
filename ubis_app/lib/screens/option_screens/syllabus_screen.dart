0a25e6e7f8fc5a7527e72374f03fa2c10fd7df8d
Furkan Ayyıldız <furkan199905@gmail.com>
2023-01-15 16:20:15 +0300

    exam schedule and syllabus screen added

--- ubis_app/lib/screens/option_screens/syllabus_screen.dart
+++ ubis_app/lib/screens/option_screens/syllabus_screen.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import "../../fake_data.dart";
 
 class SyllabusScreen extends StatelessWidget {
+  final List<String> classes = <String>[
+    "D101",
+    "T207",
+    "D403",
+    "E204",
+    "O1567"
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,8 +16,47 @@ class SyllabusScreen extends StatelessWidget {
         title: Text("Syllabus"),
         backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: Container(
-        child: Text("Syllabus Screen"),
+      body: ListView.builder(
+        itemCount: COURSE_DATA.length,
+        itemBuilder: (context, index) {
+          return Container(
+            child: Column(
+              children: [
+                Card(
+                  color: Colors.blue.shade200,
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 50, vertical: 10),
+                    child: Text("14.01.2023" + "-" + "14:00"),
+                  ),
+                ),
+                Card(
+                  elevation: 10,
+                  margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                  child: ListTile(
+                      onTap: null,
+                      title: Text(
+                          COURSE_DATA[index].code + COURSE_DATA[index].name),
+                      tileColor: Colors.grey.shade300,
+                      subtitle: Text(COURSE_DATA[index].lecturer),
+                      trailing: CircleAvatar(
+                        backgroundColor: Colors.blue.shade200,
+                        radius: 30,
+                        child: Padding(
+                          padding: const EdgeInsets.all(6),
+                          child: FittedBox(
+                            child: Text(classes[index]),
+                          ),
+                        ),
+                      )),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
